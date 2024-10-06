84daebcb164e66464ec7e869f8606f4413ece845
DhruvKhare <dhruvkumarkhare@gmail.com>
2024-10-06 11:40:59 +0530

    line 8th added locally

--- test1.py
+++ test1.py
@@ -2,9 +2,7 @@ print("Hello")
 print("test message")
 print("Hello python script")
 print("Third changes")
-<<<<<<< HEAD
 print("from local 5th line modify")
-=======
 print("from local")
 print("forth remote Changes")
->>>>>>> 619ed18a3ecc7065900d41068e49cf01671e0b2d
+print("Local branch update line 8th added")
